9d6ca860f68d30693a8a5e6d0256a425939d8cb2
jansmailbegovic <jan.smailbegovic@gmail.com>
2023-11-09 22:31:01 -0800

    fetch call

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
-
+import { useEffect, useState } from "react"
 
 const Home = () => {
+
+    const [restaurants, setRestaurants] = useState(null)
+
+    useEffect(() => {
+        const fetchRestaurants = async () => {
+            const response = await fetch('http://localhost:4000/api/call?type=italian&mood=fancy')
+            const json = await response.json();
+
+            if (response.ok) {
+                setRestaurants(json);
+            }
+        }
+        fetchRestaurants()
+    }, [])
+
     return (
         <div className="container">
-            <header>Hello World</header>
+            <header>{restaurants}</header>
         </div>
     )
 }
