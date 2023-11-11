ffe448eda007bdd28f817ad612f151ddd9a65683
jansmailbegovic <jan.smailbegovic@gmail.com>
2023-11-11 14:49:50 -0800

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div>Created by Jan S. in November 2023</div>
+            <Link to='https://github.com/jansm04'>Github</Link>
+        </footer>
+    )
+}
+
+export default Footer
