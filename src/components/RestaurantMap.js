4067843aa6657a12121104ac01af8b741e598c4f
jansmailbegovic <jan.smailbegovic@gmail.com>
2023-11-14 22:25:59 -0800

    improved readability in res-map

--- src/components/RestaurantMap.js
+++ src/components/RestaurantMap.js
@@ -5,13 +5,19 @@ import Locations from "../components/Locations"
 
 const RestaurantMap = ({ restaurants }) => {
 
+    const maps_key = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
     // toronto coordinates
     const center = { lat: 43.6632, lng: -79.3832 }
 
     return (
         <div className="res-map">
-            <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-                <Map className="google-api-map" center={center} zoom={11} disableDefaultUI={true} mapId={'adv-marker'}>
+            <APIProvider apiKey={maps_key}>
+                <Map 
+                    className="google-api-map" 
+                    center={center} 
+                    zoom={11} 
+                    disableDefaultUI={true} 
+                    mapId={'adv-marker'}>
                     {restaurants && restaurants.map((r) => (
                         <Locations restaurant={r} />
                     ))}
