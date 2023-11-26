39176c66c61a8794ecc64cb36a56332602d1f761
jansmailbegovic <jan.smailbegovic@gmail.com>
2023-11-26 00:52:59 -0800

    moved github link to name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="signature">Created by Jan S. in November 2023</div>
-            <Link className="gh-link" to='https://github.com/jansm04'>Github</Link>
+            <div className="signature">
+                Created by {<Link className="gh-link" to='https://github.com/jansm04'>Jan S.</Link>} in November 2023</div>
         </footer>
     )
 }
