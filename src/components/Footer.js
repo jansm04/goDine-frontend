e276b2971d47b673bb6b0c8be9af7759c86a5cf7
jansmailbegovic <jan.smailbegovic@gmail.com>
2023-11-25 21:53:23 -0800

    footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 const Footer = () => {
     return (
         <footer className="footer">
-            <div>Created by Jan S. in November 2023</div>
-            <Link to='https://github.com/jansm04'>Github</Link>
+            <div className="signature">Created by Jan S. in November 2023</div>
+            <Link className="gh-link" to='https://github.com/jansm04'>Github</Link>
         </footer>
     )
 }
