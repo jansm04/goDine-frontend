f530d80259f588e729ae0c1f0b71c136128a850a
jansmailbegovic <jan.smailbegovic@gmail.com>
2023-11-26 12:08:42 -0800

    null restaurant object exception handling

--- src/components/PlaceDetails.js
+++ src/components/PlaceDetails.js
@@ -9,14 +9,28 @@ const PlaceDetails = ({ restaurant }) => {
             </div>
         )
     }
+
+    if (restaurant && restaurant.id === "error") {
+        return (
+            <div className="place-details-error">
+                <p>{restaurant.displayName.text}</p>
+            </div>
+        )
+    }
  
-    return (
-        <div className="place-details">
-            <Link to={restaurant.websiteUri}>{restaurant.displayName.text}</Link>
-            <p>Rating: {restaurant.rating}</p>
-            <p>Address: {restaurant.formattedAddress}</p>
-        </div> 
-    )
+    if (restaurant && restaurant.websiteUri && restaurant.rating && restaurant.formattedAddress) {
+        return (
+            <div className="place-details">
+                <Link className="place-link" to={restaurant.websiteUri}>{restaurant.displayName.text}</Link>
+                <p className='place-rating'>{restaurant.rating}⭐️</p>
+                <p className='place-location'>📍{restaurant.formattedAddress}</p>
+            </div> 
+        )
+    } else {
+        console.log("Null restaurant object.")
+        return <></>
+    }
+    
 }
 
 export default PlaceDetails
